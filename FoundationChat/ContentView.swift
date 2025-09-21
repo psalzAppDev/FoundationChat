2dc68a7d93c18a970c2cb6a5b7abf61083a050c3
Peter Salz <psalz.app.dev@web.de>
2025-09-21 23:56:31 +0200

    Initial Commit

---
+++ FoundationChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoundationChat
+//
+//  Created by Peter Salz on 21.09.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoundationChat/FoundationChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationChatApp.swift
+//  FoundationChat
+//
+//  Created by Peter Salz on 21.09.25.
+//
+
+import SwiftUI
+
+@main
+struct FoundationChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
